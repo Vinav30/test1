18fd84b755b7ab009565cb60272bf50fc66277bd
Vinav Aggarwal <vinav30@outlook.com>
2025-07-28 23:18:26 +0530

    wapis aa gya

--- provider.tf
+++ provider.tf
@@ -2,3 +2,11 @@ resource "azurerm_resource_group" "example" {
   name     = "test"
   location = "West Europe"
 }
+resource "azurerm_resource_group" "example" {
+  name     = "test1"
+  location = "West Europe"
+}
+resource "azurerm_resource_group" "example" {
+  name     = "test2"
+  location = "West Europe"
+}
