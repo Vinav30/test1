4ee5c4465320c28ae283c889eda6bae9464ef12d
Varun Kumar <varun93.kumar@outlook.com>
2025-07-29 15:13:24 +0530

    varun's rg

--- provider.tf
+++ provider.tf
@@ -18,3 +18,8 @@ resource "azurerm_resource_group" "example" {
   name     = "test4"
   location = "West Europe"
 }
+
+resource "azurerm_resource_group" "example" {
+  name     = "varun"
+  location = "West Europe"
+}
